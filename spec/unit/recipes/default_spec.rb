8db526ecd9a1668a999beb1ba1c9ce31c76fe918
Student”git config --global user.email student@example.com” <user@knedaria2.mylabserver.com>
2017-05-09 15:16:04 +0000

    added unit tests

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -16,5 +16,38 @@ describe 'redis::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
+
+    it 'updates the package repository' do
+      expect(chef_run).to run_execute('apt-get update')
+    end
+    
+    it 'installs the necessary packages' do
+	expect(chef_run).to install_package('build-essential')
+	expect(chef_run).to install_package('tcl8.5')
+    end
+
+    it 'retrives the source code from remote location' do
+	expect(chef_run).to create_remote_file('/tmp/redis-2.8.9.tar.gz')
+    end
+
+    it 'unzips the application' do
+	#expect(chef_run).to ('/tmp/redis-2.8.9.tar.gz')
+	resource = chef_run.remote_file('/tmp/redis-2.8.9.tar.gz')
+	expect(resource).to notify('execute[tar xzf /tmp/redis-2.8.9.tar.gz]').to(:run).immediately    
+    end
+
+    it 'builds and installs the application' do
+	resource = chef_run.execute('tar xzf /tmp/redis-2.8.9.tar.gz')
+        expect(resource).to notify('execute[make && make install]').to(:run).immediately
+    end
+
+    it 'installs the redis server' do
+	resource = chef_run.execute('make && make install')
+        expect(resource).to notify('execute[echo -n | ./install_server.sh]').to(:run).immediately
+    end
+
+    it 'starts the service' do
+	expect(chef_run).to start_service('redis_6379')
+    end
   end
 end
